7f3d51a485f99399897330f7d7c00865740bc579
Omar <omarferbol@gmail.com>
2024-02-05 10:47:14 -0500

    control

--- code_camp/scientific_python/twenty_four.py
+++ code_camp/scientific_python/twenty_four.py
@@ -2,22 +2,21 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Introduction to Number theory with computing (Allemby et al., 1989)
 # 1.  prove that 24 is fascinating for the reason that it is the greatest integer for which n!
 # (factorial n) has exactly n decimal digits.
-# 2. It is also the greatest integer n which is divisible
-# by each integer not exceeding √n.
-# 3. It is the only integer n (n>0) for which
-# 1^2 + 2^2 ... + n^2 is a square
+# 2. It is the only integer n (n>0) for which 1^2 + 2^2 + ... + n^2 is a square
 num = 100
+a = np.empty(shape=(2, num))
 
 for i in range(num+1):
     facto = math.factorial(i)
-    length_num = len(str(facto))
-    if i == length_num:
-        print("The factorial of ", i, " is: ", facto, "and has: ", length_num, "digits")
+    length_facto = len(str(facto))
+    np.insert(a, i, length_facto)
+    if i == length_facto:
+        print("The factorial of ", i, " is: ", facto, "and has: ", length_facto, "digits")
     else:
-        print("num = ", i, " length = ", length_num)
+        print("num = ", i, " length = ", length_facto)
 
 # further work: compare results with kamenetsky's formula to count digits in a factorial:
 # https://thecoducer.com/2019/07/kamenetsky-formula-to-count-digits-in-factorial/
-
