bd3d25406943454ce67116cc14801b847e439939
Omar <omarferbol@gmail.com>
2024-02-08 14:12:02 -0500

    first commit and control

--- code_camp/js_data_structures_algo/spreadsheet/script.js
+++ code_camp/js_data_structures_algo/spreadsheet/script.js
@@ -1,6 +1,8 @@
-const range = (start, end) => Array(end - start + 1).fill(start).map((element, index)=>element + index);
+const range = (start, end) => Array(end - start + 1).fill(start);
+//.map((element, index)=>element + index);
 
-const charRange = (start, end) => range(start, end);
+//convert start and end to charaters
+const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map((code)=>String.fromCharCode(code));
 
 window.onload = () => {
     const container = document.getElementById("container");
@@ -10,5 +12,10 @@ window.onload = () => {
         label.textContent = name;
         container.appendChild(label);
     };
+    const letters = charRange('A', 'J');
+    letters.forEach(createLabel);
 };
+
+console.log("output of range: ", range(1,4));
+//console.log("output of charRange: ", charRange('A','D'));
 
