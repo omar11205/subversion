05de626033ee15010981a198d88205b34bc7fde9
Omar <omarferbol@gmail.com>
2024-02-08 20:17:57 -0500

    control

--- code_camp/js_data_structures_algo/spreadsheet/script.js
+++ code_camp/js_data_structures_algo/spreadsheet/script.js
@@ -1,5 +1,26 @@
-const range = (start, end) => Array(end - start + 1).fill(start);
-//.map((element, index)=>element + index);
+const isEven = (num) => num % 2 === 0 ? true : false;
+
+const sum = (nums) => nums.reduce((acc, el) => acc + el, 0);
+
+const average = (nums) => sum(nums)/nums.length;
+
+
+
+
+const median = nums => {
+    const sorted = nums.slice().sort((a, b) => a - b);
+    const length = sorted.length;
+    const middle = length / 2 - 1;
+    return isEven(length) ? average([sorted[middle], sorted[middle + 1]]) : sorted[Math.ceil(middle)];
+}
+
+const spreadsheetFunctions = {
+    sum,
+    average,
+    median
+}
+
+const range = (start, end) => Array(end - start + 1).fill(start).map((element, index)=>element + index);
 
 //convert start and end to charaters
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map((code)=>String.fromCharCode(code));
@@ -14,8 +35,18 @@ window.onload = () => {
     };
     const letters = charRange('A', 'J');
     letters.forEach(createLabel);
+    range(1, 99).forEach(number => {
+        createLabel(number);
+        letters.forEach(letter => {
+          const input = document.createElement("input");
+          input.type = "text";
+          input.id = letter + number;
+          input.ariaLabel = letter + number;
+          container.appendChild(input);
+        })
+    })
 };
 
 console.log("output of range: ", range(1,4));
-//console.log("output of charRange: ", charRange('A','D'));
+console.log("output of charRange: ", charRange('A','D'));
 
