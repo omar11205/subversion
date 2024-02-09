70ed0d62efbfd5ed2dbc4bcc65c2ec19905141c2
Omar <omarferbol@gmail.com>
2024-02-09 15:27:13 -0500

    control

--- code_camp/js_data_structures_algo/spreadsheet/script.js
+++ code_camp/js_data_structures_algo/spreadsheet/script.js
@@ -4,9 +4,6 @@ const sum = (nums) => nums.reduce((acc, el) => acc + el, 0);
 
 const average = (nums) => sum(nums)/nums.length;
 
-
-
-
 const median = nums => {
     const sorted = nums.slice().sort((a, b) => a - b);
     const length = sorted.length;
@@ -25,6 +22,18 @@ const range = (start, end) => Array(end - start + 1).fill(start).map((element, i
 //convert start and end to charaters
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map((code)=>String.fromCharCode(code));
 
+const evalFormula = (x, cells) => {
+    const idToText = id => cells.find(cell => cell.id === id).value;
+    const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+    const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+    const elemValue = num => {
+      const inner = character => {
+  
+      }
+      return inner;
+    }
+  }
+
 window.onload = () => {
     const container = document.getElementById("container");
     const createLabel = (name) => {
@@ -42,11 +51,17 @@ window.onload = () => {
           input.type = "text";
           input.id = letter + number;
           input.ariaLabel = letter + number;
+          input.onchange = update;
           container.appendChild(input);
         })
     })
 };
 
+const update = (event) => {
+    const element = event.target;
+    const value = element.value.replace(/\s/g, "");
+    if (!value.includes(element.id) && value.startsWith('=')){}
+}
 console.log("output of range: ", range(1,4));
 console.log("output of charRange: ", charRange('A','D'));
 
