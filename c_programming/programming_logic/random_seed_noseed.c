1cb1defe135c46a6ec4174d2fb3c41e42213e105
Omar <omarferbol@gmail.com>
2024-02-22 15:15:32 -0500

    control

--- c_programming/programming_logic/random_seed_noseed.c
+++ c_programming/programming_logic/random_seed_noseed.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /*generate alleatory whole numbers between 0 to RAND_MAX (32000)*/
 /* num = A + rand() % (B - A + 1)*/
-
-int *aleatoryNoSeed(int a, int b, int many){ //a left limit, b right limit
+/* the function aleatory will return a pointer to an array of randomly generated numbers or NULL is there any problem with the parameters in the calling */
+int *aleatory(int seed, int a, int b, int many){ //a left limit, b right limit,  
+	int *res;
+	if(b<a || b == a){ //control for the 
+		printf("Interval error: b<a or b=a \n");
+		void *res = malloc(sizeof(int)); //recasting res to a void pointer to return NULL instead 0 
+		res = NULL;
+		return res;
+	} else {
+		res = (int*)malloc(many*sizeof(int));
+	}
+	
 	int i;
-	int *res = (int*)malloc(many*sizeof(int)); 
+	
+	if(seed == 1){
+		printf("Alleatory seed ON\n");
+		srand(time(NULL));	
+	} else {
+		printf("Alleatory seed OFF\n");
+	}
 	
 	for (i = 0; i<many; i++){
 		res[i] =  a + rand()%(b-a+1);
@@ -14,17 +31,22 @@ int *aleatoryNoSeed(int a, int b, int many){ //a left limit, b right limit
 	return res;
 	
 }
-
+	
 int main(void) {
-	int a = 1;
-	int b = 6;
+	int a = 20;
+	int b = 10;
 	int many = 20;
 	int i;
-	int *p = aleatoryNoSeed(a,b,many);
-	for(i = 0; i<many; i++){
-		printf("Aleatory number [%d] = %d\n", i+1, p[i]);
+	int *p;
+	
+	p = aleatory(1, a, b, many);
+	if (p != NULL){
+		for(i = 0; i<many; i++){
+			printf("Aleatory number seed [%d] = %d\n", i+1, p[i]);
+		}
+	} else {
+		printf("Alleatory generation failed\n");
 	}
 	free(p);
 	return 0;
 }
-
