9e9012dfc6116d16ff2d3016b173e381a4cf811c
Omar <omarferbol@gmail.com>
2024-02-23 20:07:31 -0500

    proyect finished and corrected

--- code_camp/js_data_structures_algo/cash_register_app/script.js
+++ code_camp/js_data_structures_algo/cash_register_app/script.js
@@ -1,7 +1,11 @@
 const changeDue = document.getElementById('change-due');
 const cashEl = document.getElementById('cash');
+const purchaseBtn = document.getElementById('purchase-btn');
+const currentPricep = document.getElementById('price');
 
-let price = 3.26;
+let price = 19.5;
+currentPricep.innerText = `Current article price $${price}`;
+
 let cid = [
   ["PENNY", 1.01],
   ["NICKEL", 2.05],
@@ -58,9 +62,12 @@ const checkCashRegister = (price, cash, cid) => {
   return { status: "OPEN", change: changeArray};
 }
 
-document.getElementById('purchase-btn').addEventListener('click', function() {
+const displayIterator = (element) => {
+  changeDue.textContent += ` ${element[0]}: $${element[1]}`
+};
+
+purchaseBtn.addEventListener('click', function() {
   const cash = parseFloat(cashEl.value);
-  console.log(cash);
   if (isNaN(cash)) {
     alert('Please enter a valid cash amount.');
     return;
@@ -72,8 +79,14 @@ document.getElementById('purchase-btn').addEventListener('click', function() {
     changeDue.textContent = "No change due - customer paid with exact cash";
   } else if (cash > price){
     let objResult = checkCashRegister(price, cash, cid);
-    console.log(objResult);
-    //changeDue.textContent =   
+    if(objResult.status === "INSUFFICIENT_FUNDS"){
+      changeDue.textContent = `Status: ${objResult.status}`;
+    } else if (objResult.status === "CLOSED"){
+      changeDue.textContent = `Status: ${objResult.status}`
+      objResult.change.forEach(displayIterator);
+    } else if (objResult.status === "OPEN"){
+      changeDue.textContent = `Status: ${objResult.status}`
+      objResult.change.forEach(displayIterator);
+    }  
   }
 });
-
