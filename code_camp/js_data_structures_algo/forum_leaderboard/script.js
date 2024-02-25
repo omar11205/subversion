4d36c5262b375c35fe816db59c10d8ce21306312
Omar <omarferbol@gmail.com>
2024-02-24 22:17:13 -0500

    proyect finished

--- code_camp/js_data_structures_algo/forum_leaderboard/script.js
+++ code_camp/js_data_structures_algo/forum_leaderboard/script.js
@@ -48,8 +48,71 @@ const timeAgo = (time) => {
     const lastPost = new Date(time);
     const timeDifference = currentTime - lastPost;
     const msPerMinute = 1000*60;
+    const minutesAgo = Math.floor(timeDifference/msPerMinute);
+    const hoursAgo = Math.floor(minutesAgo/60);
+    const daysAgo = Math.floor(hoursAgo/24);
+    if (minutesAgo < 60){
+        return `${minutesAgo}m ago`;
+    }
+    if (hoursAgo<24){
+        return `${hoursAgo}h ago`;
+    }
+    return `${daysAgo}d ago`;
 };
 
+/*You need a function to convert view counts to a more readable format. For example, if the view count is 1000, it should display as 1k and if the view count is 100,000 it should display as 100k.*/
+
+const viewCount = (views) => {
+    const thousands = Math.floor(views/1000);
+    if (views >= 1000){
+        return `${thousands}k`;
+    }
+    return views;
+};
+
+/* Each of the forum topics includes a category like Python or JavaScript. In the next few steps, you will build out a category object which holds all of the forum categories and classNames for the styling. */
+
+const allCategories = {
+    299: {category: "Career Advice", className: "career"},
+    409: { category: "Project Feedback", className: "feedback" },
+    417: {category: "freeCodeCamp Support", className: "support"},
+    421: { category: "JavaScript", className: "javascript" },
+    423: { category: "HTML - CSS", className: "html-css" },
+    424: { category: "Python", className: "python" },
+    432: { category: "You Can Do This!", className: "motivation" },
+    560: { category: "Backend Development", className: "backend" },
+};
+
+const forumCategory = (id) => {
+    let selectedCategory = {};
+    if (allCategories.hasOwnProperty(id)){
+        const {className, category} = allCategories[id];
+        selectedCategory.className = className;
+        selectedCategory.category = category;
+    } else {
+        selectedCategory.className = "general";
+        selectedCategory.category = "General";
+        selectedCategory.id = 1;
+    }
+
+    const url = `${forumCategoryUrl}${selectedCategory.className}/${id}`;
+    const linkText = selectedCategory.category;
+    const linkClass = `category ${selectedCategory.className}`;
+    return `<a href="${url}" class="${linkClass}" target="_blank">${linkText}</a>`;
+};
+
+/*Each forum post will include a list of user avatar images which represent all of the users participating in the conversation for that topic.*/
+
+const avatars = (posters, users) => {
+    return posters.map((poster)=>{
+        const user = users.find((user)=>user.id===poster.user_id);
+        if(user){
+            const avatar = user.avatar_template.replace(/{size}/, 30);
+            const userAvatarUrl = avatar.startsWith("/user_avatar/") ? avatarUrl.concat(avatar) : avatar;
+            return `<img src="${userAvatarUrl}" alt="${user.name}">`;
+        }
+    }).join("");
+};
 
 const showLatestPosts = (data) => {
     const {topic_list, users} = data;
@@ -60,11 +123,14 @@ const showLatestPosts = (data) => {
         <tr>
             <td>
                 <p class="post-title">${title}</p>
+                ${forumCategory(category_id)}
             </td>
-            <td></td>
+            <td>
+                <div class="avatar-container">${avatars(posters, users)}</div>
+            </td>
             <td>${posts_count - 1}</td>
-            <td>${views}</td>
-            <td></td>
+            <td>${viewCount(views)}</td>
+            <td>${timeAgo(bumped_at)}</td>
         </tr>
         `;
     }).join("");
