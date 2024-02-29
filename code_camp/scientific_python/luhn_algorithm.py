04c0201e1cccb6f98c86dadee681d330a5b4a3a1
Omar <omarferbol@gmail.com>
2024-02-28 22:22:40 -0500

    control

--- code_camp/scientific_python/luhn_algorithm.py
+++ code_camp/scientific_python/luhn_algorithm.py
@@ -13,11 +13,16 @@ def verify_card_number(card_number):
     card_number_start_to_end = card_number[::]
     card_number_last_four_reversed = card_number[-1:-5:-1]
     card_number_reversed = card_number[::-1]
-    print(card_number_reversed)
+    odd_digits = card_number_reversed[::2]
+    for digit in odd_digits:  # "for each element of odd_digits as char"
+        # type error, trying to add a string to an integer for that use int(digit)
+        sum_of_odd_digits += int(digit)
+        print(sum_of_odd_digits)
 
 
 def main():
     card_number = '4111-1111-4555-1142'
+    # replace in the string hyphens and blank spaces for empty spaces
     card_translation = str.maketrans({'-': '', ' ': ''})
     translated_card_number = card_number.translate(card_translation)
 
