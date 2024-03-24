bd817e4cc2d4319c2b7133bcab261491424b7ae6
Omar <omarferbol@gmail.com>
2024-03-23 23:02:47 -0500

    finishing hanoi tower project

--- code_camp/scientific_python/hanoi_tower.py
+++ code_camp/scientific_python/hanoi_tower.py
@@ -6,7 +6,7 @@
     You cannot place larger disks on top of smaller ones
 
 """
-NUMBER_OF_DISKS = 4
+NUMBER_OF_DISKS = 5
 # minimum theoretical number of moves = 2^n - 1, where n = number of disks
 number_of_moves = 2**NUMBER_OF_DISKS - 1
 
@@ -15,6 +15,10 @@ rods = {
     'B': [],
     'C': []
 }
+
+A = list(range(NUMBER_OF_DISKS, 0, -1))
+B = []
+C = []
 
 
 # this algorithm controls the movements from one disk to another
@@ -82,14 +86,44 @@ def move_four(n, source, auxiliary, target):
             make_allowed_move(auxiliary, target)
 
 
+"""
+    To solve the puzzle for any n with recursion, the first thing to do is break the original problem down into smaller 
+    sub-problems.
+
+    The final configuration with n disks piled up to the third rod in decreasing order can be obtained by moving:
+
+    n - 1 disks from the source to the auxiliary rod
+    the largest disk from the source to the target
+    and then the n - 1 disks from the auxiliary rod to the target.
+    So, the first thing the move function should do is calling itself with n - 1 as the first argument. 
+    But if you try to do so without defining a base case, you will get a RecursionError. 
+    This happens because the function keeps calling itself indefinitely
+"""
+
+
 def move_general(n, source, auxiliary, target):
-    pass
+    if n <= 0:
+        return
+
+    # move n-1 disks from source to auxiliary, so they are out of the way
+    move_general(n-1, source, target, auxiliary)
+
+    # move the nth disk from source to target
+    target.append(source.pop())
+
+    # display starting configuration
+    print(A, B, C, '\n')
+
+    # move the n - 1 disks that we left on auxiliary onto target
+    move_general(n - 1, auxiliary, source, target)
+
 
 # initiate call from source A to target C with auxiliary B (n=3)
 # move(NUMBER_OF_DISKS, 'A', 'B', 'C')
 
 # initiate call from source A to target C with auxiliary B (n=4)
-move_four(NUMBER_OF_DISKS, 'A', 'B', 'C')
+# move_four(NUMBER_OF_DISKS, 'A', 'B', 'C')
 
-
+# initiate call from source A to target C with auxiliary B (n)
+move_general(NUMBER_OF_DISKS, A, B, C)
 
