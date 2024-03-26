4c9564b92e70e49e733235bd423fa81aa4451815
ombolanos <omarferbol@gmail.com>
2024-03-26 00:58:12 -0500

    finishing merge sort algorithm

--- code_camp/scientific_python/merge_sort.py
+++ code_camp/scientific_python/merge_sort.py
@@ -9,6 +9,8 @@
 
 
 def merge_sort(array):
+    if len(array) <= 1:
+        return
     # // = integer division operator
     middle_point = len(array) // 2
     # using left syntax extract the left half of array.
@@ -25,3 +27,44 @@ def merge_sort(array):
     right_part = array[middle_point:]
     # recursive merge sort to break the array in to individual parts
     merge_sort(left_part)
+    merge_sort(right_part)
+    """
+    Now it's time to sort and merge the lists (left_part and right_part) into the original array.
+    You can do this by comparing elements on both lists, and merging the smaller element to the main list. 
+    You are going to do this comparison for all the indexes in left_part and right_part.
+    
+    """
+    left_array_index = 0
+    right_array_index = 0
+    sorted_index = 0
+
+    # compares one element from left_part with another in right_part, then adds the smaller element
+    # to the main array list
+    while left_array_index < len(left_part) and right_array_index < len(right_part):
+        if left_part[left_array_index] < right_part[right_array_index]:
+            array[sorted_index] = left_part[left_array_index]
+            left_array_index += 1
+        else:
+            array[sorted_index] = right_part[right_array_index]
+            right_array_index += 1
+        sorted_index += 1
+
+    # copy the remaining elements in left_part into the array list.
+    while left_array_index < len(left_part):
+        array[sorted_index] = left_part[left_array_index]
+        left_array_index += 1
+        sorted_index += 1
+
+    # copy the remaining elements in right_part into the array list.
+    while right_array_index < len(right_part):
+        array[sorted_index] = right_part[right_array_index]
+        right_array_index += 1
+        sorted_index += 1
+
+
+if __name__ == '__main__':
+    numbers = [4, 10, 6 , 14, 2, 1, 8, 5]
+    print("Unsorted Array:")
+    print(numbers)
+    merge_sort(numbers)
+    print('Sorted array: ' + str(numbers))
