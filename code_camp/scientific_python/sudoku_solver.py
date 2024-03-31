9ff83446121c2fbea86c4b7264b25a4430eaa4c0
ombolanos <omarferbol@gmail.com>
2024-03-30 22:18:35 -0500

    control

--- code_camp/scientific_python/sudoku_solver.py
+++ code_camp/scientific_python/sudoku_solver.py
@@ -5,3 +5,16 @@ class Board:
     def __str__(self):
         upper_lines = f'\n╔═══{"╤═══" * 2}{"╦═══"}{"╤═══" * 2}{"╦═══"}{"╤═══" * 2}╗\n'
         middle_lines = f'╟───{"┼───" * 2}{"╫───"}{"┼───" * 2}{"╫───"}{"┼───" * 2}╢\n'
+        lower_lines = f'╚═══{"╧═══" * 2}{"╩═══"}{"╧═══" * 2}{"╩═══"}{"╧═══" * 2}╝\n'
+        board_string = upper_lines
+
+        """
+        Enumeration is a convenient way to keep track of both the element and its position on a list. The enumerate() 
+        function is a built-in function in Python that takes an iterable (such as a list, tuple, or string) and returns 
+        an iterator that produces tuples containing indices and corresponding values from the iterable.
+        """
+        for index, line in enumerate(self.board):
+            row_list = []
+            for square_no, part in enumerate([line[:3], line[3:6], line[6:]], start=1):
+                
+
--- cpp_programming/programming_logic/buritica_programming_logic_ch_7_exe.cpp
+++ cpp_programming/programming_logic/buritica_programming_logic_ch_7_exe.cpp
@@ -1,25 +1,48 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-bool endsWithFour(int number) {
-	// Get the last digit of the number
-	int lastDigit = number % 10;
-	// Check if the last digit is 4
-	return (lastDigit == 4);
-}
-
-int main() {
+void endsWithNumber(int endsWith) {
+	//Read an integer number and determine if it's a number that ends with 4
 	int number;
 	cout << "Enter an integer: ";
 	cin >> number;
-	
-	if (endsWithFour(number)) {
-		cout << "The last digit of " << number << " is 4." << endl;
+	// Get the last digit of the number
+	int lastDigit = number % 10;
+	// Check if the last digit is 4
+	if (lastDigit == endsWith) {
+		cout << "The last digit of " << number << " is " << endsWith << endl;
 	} else {
-		cout << "The last digit of " << number << " is not 4." << endl;
+		cout << "The last digit of " << number << " is not " << endsWith << endl;
 	}
-	
+	return;
+}
+
+void haveNDigits(int n){
+	//Read an integer number and determine if it have 3 digits
+	int num;
+	int temp;
+	int counter = 0;
+	cout << "Provide an integer number: ";
+	cin >> num;
+	num = abs(num);
+	temp = abs(num);
+	while (num > 0) {
+		num = num / 10;
+		counter++;
+	}
+	if (counter == n){
+		cout << "The number of digits of " << temp << " is " << n << endl;
+	} else {
+		cout << "The number of digits of " << temp << " is not " << n << endl;
+	}
+	return;
+}
+
+int main() {
+	//haveNDigits(3);
+	//endsWithNumber(4);
 	return 0;
 }
 
