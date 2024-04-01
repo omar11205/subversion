8442ec7b7a66755bebc1fded0ef2cc677856815e
ombolanos <omarferbol@gmail.com>
2024-04-01 14:45:30 -0500

    control

--- code_camp/scientific_python/sudoku_solver.py
+++ code_camp/scientific_python/sudoku_solver.py
@@ -55,6 +55,24 @@ class Board:
         # and return True, otherwise return false
         all(self.board[row][col] != num for row in range(9))
 
-    def valid_in_square(self, row, col, num):
+    # method to check if the number is valid (not repeated) in a 3x3 square
+    def valid_in_square(self, row, col, num):  # row = row index, col = column index, num = number to be checked
+        """
+            Now you need to calculate the starting row index for the 3x3 block in the board grid. For that, ensure that
+            the starting row index for each 3x3 block is a multiple of 3. This can be achieved by this mathematical
+            operation: (row // 3) * 3.
+        """
+        row_start = (row // 3) * 3  # check for similar logic in (Buritica, p 134, 2023)
+        col_start=(col // 3) * 3
+        for row_no in range(row_start, row_start + 3):
+            for col_no in range(col_start, col_start + 3):
+                if self.board[row_no][col_no] == num:
+                    return False
+        return True
+
+    # method to check if a given number is a valid choice for an empty cell in the sudoku board by validating its
+    # compatibility wi the row, column, and 3x3 square of the specified empty cell
+    def is_valid(self, empty, num):  # empty = a tuple representing the row and column indices of an empty cell, num =
+        # number to be checked
         pass
 
