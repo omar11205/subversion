ba604399241c19fcb34f65627505a12a10d11bb7
ombolanos <omarferbol@gmail.com>
2024-04-01 22:37:56 -0500

    control

--- code_camp/scientific_python/tree_traversal_binary_search_tree.py
+++ code_camp/scientific_python/tree_traversal_binary_search_tree.py
@@ -4,6 +4,7 @@ class TreeNode:
         self.left = None
         self.right = None
 
+
 class BinarySearchTree:
     def __init__(self):
         # root = root node of the binary search tree
@@ -32,3 +33,34 @@ class BinarySearchTree:
             node.right = self._insert(node.right, key)
         return node
 
+    def search(self, key):
+        return self._search(self.root, key)
+
+    def _search(self, node, key):
+        if node is None or node.key == key:
+            return node
+        if key < node.key:
+            return self._search(node.left, key)
+        return self._search(node.right, key)
+
+    def delete(self, key):
+        self.root = self._delete(self.root, key)
+
+    def _delete(self, node, key):
+        if node is None:
+            return node
+        if key < node.key:
+            node.left = self._delete(node.left, key)
+        elif key > node.key:
+            node.right = self._delete(node.right, key)
+        else:
+            if node.left is None:
+                return node.right
+
+            elif node.right is None:
+                return node.left
+
+            node.key = self._min_value(node.right)
+            node.right = self._delete(node.right, node.key)
+        return node
+    
