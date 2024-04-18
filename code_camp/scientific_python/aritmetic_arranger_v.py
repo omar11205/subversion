e54def1c6e774e6242b6f174987a9da3cf21e291
Omar <omarferbol@gmail.com>
2024-04-17 19:19:52 -0500

    finished project

--- code_camp/scientific_python/aritmetic_arranger_v.py
+++ code_camp/scientific_python/aritmetic_arranger_v.py
@@ -16,7 +16,7 @@ def arithmetic_arranger(problems, show_answers=False):
         first_number, operator, second_number = problem.split(' ')
 
         if operator not in allowed_operators:
-            return "Error: Operator must be '+' or '-"
+            return "Error: Operator must be '+' or '-'."
 
         only_contains_digits = first_number.isdigit() and second_number.isdigit()
         if not only_contains_digits:
@@ -41,35 +41,60 @@ def arithmetic_arranger(problems, show_answers=False):
             elif operators[i] == '-':
                 results.append(first_operators_int[i] - second_operators_int[i])
 
-    max_len_spacing = []
-    string_result = ''
-    first_operands_string = ''
-    spacing = ' '*4
+    max_len_spacing = []  # stores the max length
     for i in range(len(operators)):
         max_len_spacing.append(max(len(first_operators[i]), len(second_operators[i]))+1)
 
-    fo_dashed_list = []
+    fo_spaced_list = []   # first operators with the required spacing
+    so_spaced_list = []   # second operators with the required spacing
+    re_spaced_list = []   # results with the required spacing
     for i in range(len(max_len_spacing)):
-        # Using f-string (Python 3.6+)
-        dash_str = '-' * max_len_spacing[i]
-        num_to_replace = len(dash_str) - len(first_operators[i])
-        dashed_string = dash_str[:num_to_replace] + first_operators[i]
-        fo_dashed_list.append(dashed_string)
+        fo_align_numb_str = f" {first_operators[i]:>{max_len_spacing[i]}}"
+        so_align_numb_str = f"{second_operators[i]:>{max_len_spacing[i]}}"
+        fo_spaced_list.append(fo_align_numb_str)
+        so_spaced_list.append(so_align_numb_str)
+        if show_answers:
+            so_align_numb_str = f" {results[i]:>{max_len_spacing[i]}}"
+            re_spaced_list.append(so_align_numb_str)
 
-    for i, dashed in enumerate(fo_dashed_list):
-        if i != len(fo_dashed_list) - 1:  # Verificar si no es el último elemento
-            first_operands_string += str(dashed) + spacing  # Agregar el número seguido de cuatro espacios a la cadena
+    first_line_string = ''
+    spacing = ' ' * 4
+    for i, spaced in enumerate(fo_spaced_list):
+        if i != len(fo_spaced_list) - 1:
+            first_line_string += str(spaced) + spacing
         else:
-            first_operands_string += str(dashed)  # Agregar el último número sin espacios adicionales al final
+            first_line_string += str(spaced)
 
-    # for i in range(len(max_len_spacing)):
-    #     string_result += max_len_spacing[i] +
+    second_line_string = ''
+    for i, spaced in enumerate(so_spaced_list):
+        if i != len(so_spaced_list) - 1:
+            second_line_string += operators[i] + str(spaced) + spacing
+        else:
+            second_line_string += operators[i] + str(spaced)
 
-    print(fo_dashed_list)
-    print(first_operands_string)
-    
+    dashed_line_str = ''
+    for i in range(len(max_len_spacing)):
+        if i != len(max_len_spacing) - 1:
+            dashed_line_str += '-'*(max_len_spacing[i]+1) + spacing
+        else:
+            dashed_line_str += '-'*(max_len_spacing[i]+1)
 
-arithmetic_arranger(['34 + 1546', '1 - 134', '12 - 1000', '1000 + 900'])
+    results_line_str = ''
+    for i, spaced in enumerate(re_spaced_list):
+        if i != len(re_spaced_list) - 1:
+            results_line_str += str(spaced) + spacing
+        else:
+            results_line_str += str(spaced)
+
+    if show_answers:
+        arranged_problems = first_line_string + '\n' + second_line_string + '\n' + dashed_line_str + '\n' + results_line_str
+    else:
+        arranged_problems = first_line_string + '\n' + second_line_string + '\n' + dashed_line_str
+
+    return arranged_problems
+
+
+print(arithmetic_arranger(["3801 - 2", "123 + 49"]))
 
 
 
