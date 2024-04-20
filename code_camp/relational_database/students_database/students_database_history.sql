355c989c4ab79a8860fc376c356d87a339455d67
Omar <omarferbol@gmail.com>
2024-04-20 11:39:57 -0500

    control

--- code_camp/relational_database/students_database/students_database_history.sql
+++ code_camp/relational_database/students_database/students_database_history.sql
@@ -55,3 +55,4 @@ INSERT INTO majors_courses(major_id, course_id) VALUES(1,1);
 
 --filling students table
 INSERT INTO students(first_name, last_name, major_id, gpa) VALUES('Rhea', 'Kellems', 1, 2.5);
+
